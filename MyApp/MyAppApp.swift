975ad997548d8f48441ec57d44911e5f6018de3e
藤田将豪 <tengtianjiang9@gmail.com>
2023-05-29 12:59:55 +0900

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by 藤田将豪 on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
